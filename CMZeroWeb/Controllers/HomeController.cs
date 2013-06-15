a5fad67f5db5e9ba4eaa1e9848a0fbfa701d76ba
Andrew Metcalfe <ametcalfe@opentable.com>
2013-06-15 19:12:21 +0100

    Website showing the styles in a basic form

---
+++ CMZeroWeb/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Web.Mvc;
+
+namespace CMZeroWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
